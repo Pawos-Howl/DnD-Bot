3d39a9955ad8941bc324294206086681076ba83e
Pawos-Howl <pawoshowl@gmail.com>
2023-05-25 12:50:41 -0700

    Current Commit

--- main.py
+++ main.py
@@ -12,53 +12,48 @@ intents.guild_messages = False
 client = commands.Bot(command_prefix=":3",intents=intents)
 ADMIN_IDS = [979210001556070491, 809870005914566676, 675559827425984582]
 
-NatReactMessage = False
-NatReactEmote = False
-
-# why
-#guildsopen = []
+NatReactMessage = True
+NatReactEmote = True
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    #try:
     for guild in client.guilds:
-
-        #guildsopen.append(v.name)
+        client.tree.copy_global_to(guild=guild)
         await client.tree.sync(guild=guild)
-        # print(f'synced {len(synced)} command(s)')
     
-    # Dont do that error handler, or at least make it a specific error
-
-    # except Exception as exception:
-    #     print(exception)
-
 @client.tree.command()
-async def roll(interaction: discord.Interaction, dice_sides: int, number_of_dice: int = 1, add: int = None, multiply: int = None):
+async def roll(interaction: discord.Interaction, dice_sides: int, reason: str = None, number_of_dice: int = 1, add: int = None, multiply: int = None):
     # a natural 1, ouch man...(doggy); bad luck?(doggy); lets hope for a better roll next time.(doggy); yay, a nat 1 /j(doggy); Gotta wish you were a halfling(james); 
-    # nat1_reactList = ["A natural 1, ouch man...","Bad luck?","Lets hope for a better roll next time.","yay, a nat 1 /j","Gotta wish you werer a halfling."]
-    # nat1_reactAuth = ["Samaris","Samaris","Samaris","Samaris","James"]
-    # nat20_reactList = ["..."]
-    # nat20_reactAuth = ["..."]
+    nat1_reactList = ["A natural 1, ouch man...","Bad luck?","Lets hope for a better roll next time.","yay, a nat 1 /j","Gotta wish you werer a halfling."]
+    nat1_reactAuth = ["Samaris","Samaris","Samaris","Samaris","James"]
+    nat20_reactList = ["..."]
+    nat20_reactAuth = ["..."]
 
     amount_of_dice = number_of_dice
     dice_add = add
     dice_multiply = multiply
     dice_sum = 0
     dice_rolls = []
+    appendRoll = ''
 
     # "Rolls" the dice
     for i in range(amount_of_dice):
         randomNumber = random.randint(1, dice_sides)
         dice_rolls.append(randomNumber)
         dice_sum += randomNumber
+    
+    #REMOVE THIS ITS RIGGING!
+    dice_sum = 1
+    dice_rolls = [1]
+    print("THIS RESULT IS RIGGED")
+    appendRoll += "THIS WAS A RIGGED RESULT; "
 
     if add != None: dice_sum += add
     if multiply != None: dice_sum = int(dice_sum)*int(multiply)
 
     # Gives a "nice" looking output
-    if len(dice_rolls) == 1 and dice_multiply == None and dice_add == None: return f'The random number is {dice_sum}'
-    appendRoll = ''
+    if reason != None: appendRoll += f'Rolled for: {reason}\n'
     if dice_add != None: appendRoll += f'Added:{dice_add}; '
     if dice_multiply != None: appendRoll += f'Multiplied:{dice_multiply}; '
     if len(dice_rolls) == 1: appendRoll += f'Roll:{dice_rolls[0]}'
@@ -70,29 +65,30 @@ async def roll(interaction: discord.Interaction, dice_sides: int, number_of_dice
         appendRoll += f'Rolls:{rollsAppend}'
     msg =  f'The result is {dice_sum}\n{appendRoll}'
     # Post work for if the result was either nat20/20 or nat1
-    # Use this! https://www.w3schools.com/python/python_strings_format.asp
-    # if NatReactMessage and len(dice_rolls) == 1:
-    #     rollReact = ''
-    #     if dice_rolls[0] == 1:
-    #         #This is a nat1
-    #         natReaction = nat1_reactList[random.randint(0, len(nat1_reactList-1))]
-    #         natReactionAuthor = nat1_reactAuth[nat1_reactList.index(natReaction)]
-    #         rollReact = f'\n{natReaction} - {natReactionAuthor}'
-    #     if dice_rolls[0] == 20 and dice_sides == 20:
-    #         #This is a nat20/20
-    #         natReaction = nat20_reactList[random.randint(0, len(nat20_reactList-1))]
-    #         natReactionAuthor = nat20_reactAuth[nat20_reactList.index(natReaction)]
-    #         rollReact = f'\n{natReaction} - {natReactionAuthor}'
-    #     msg = msg + rollReact
-    # interactionMessage = await interaction.response.send_message(msg)
-    # if NatReactEmote and len(dice_rolls) == 1:
-    #     if dice_rolls[0] == 1:
-    #         #This is a nat 1
-    #         await interactionMessage.add_reaction('1108177281249189938')
-    #     if dice_rolls[0] == 20 and dice_sides == 20:
-    #         #This is a nat20/20
-    #         await interactionMessage.add_reaction('1108177315663466586')
+    if NatReactMessage and len(dice_rolls) == 1:
+        rollReact = ''
+        if dice_rolls[0] == 1:
+            #This is a nat1
+            randPlace = random.randint(1, (len(nat1_reactList)-1))
+            natReaction = nat1_reactList[randPlace]
+            natReactionAuthor = nat1_reactAuth[randPlace]
+            rollReact = f'\n{natReaction} - {natReactionAuthor}'
+        if dice_rolls[0] == 20 and dice_sides == 20:
+            #This is a nat20/20
+            randPlace = random.randint(1, (len(nat20_reactList)-1))
+            natReaction = nat20_reactList[randPlace]
+            natReactionAuthor = nat20_reactAuth[randPlace]
+            rollReact = f'\n{natReaction} - {natReactionAuthor}'
+        msg = msg + rollReact
     await interaction.response.send_message(msg)
+    message = await interaction.original_response()
+    if NatReactEmote and len(dice_rolls) == 1:
+        if dice_rolls[0] == 1:
+            #This is a nat 1
+            await message.add_reaction("<:nat1:1108177281249189938>")
+        if dice_rolls[0] == 20 and dice_sides == 20:
+            #This is a nat20/20
+            await message.add_reaction("<:nat20:1108177315663466586>")
 
 @client.tree.command()
 async def stopbot(interaction: discord.Interaction):
